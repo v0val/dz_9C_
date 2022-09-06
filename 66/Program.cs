84e5057f7eb9939bb19a0ed07f734c9c768329e5
valeryO <olennval@mail.ru>
2022-09-07 00:00:15 +0700

    number 66

---
+++ 66/Program.cs
@@ -0,0 +1,12 @@
+﻿int summ = 0;
+int sumChisel(int m, int n)
+{
+    if (m == n + 1) return summ;
+    summ += m;
+    sumChisel(m + 1, n);
+    return summ;
+}
+Console.Write("Введи целые числа х и у (х<y) ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write(sumChisel(x, y));
