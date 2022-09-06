2f05537d24b1cfc620be6d915d72abb850fcf9fe
valeryO <olennval@mail.ru>
2022-09-07 00:27:29 +0700

    number 68

---
+++ 68/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи целые неотрицательные числа х и у (х<y) ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int AkkermanFunc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else 
+    {
+         if (m != 0 && n == 0) return AkkermanFunc(m - 1, 1);
+         else 
+         {
+            return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1 ));
+         }
+    }
+        
+}
+Console.Write(AkkermanFunc(x, y));
