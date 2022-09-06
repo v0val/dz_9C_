592715b4376b31cf1b415e314d4bd09d3d73db4a
valeryO <olennval@mail.ru>
2022-09-06 23:42:42 +0700

    number 64

---
+++ 64/Program.cs
@@ -0,0 +1,9 @@
+﻿void ryadChisel(int n, int m)
+{
+    if (m == n + 1) return;
+    ryadChisel(n, m + 1);
+    Console.Write($"{m} ");
+}
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 1;
+ryadChisel(x, y);
